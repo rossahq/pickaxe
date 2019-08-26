81f63d481107394a2da89cf5ace3f85b1f5e0805
rossahq <rossahq@gmail.com>
2019-08-26 16:05:50 +0100

    find most common words implemented

--- argumentMining/miningtools.py
+++ argumentMining/miningtools.py
@@ -3,18 +3,16 @@ from fuzzywuzzy import fuzz
 
 class MiningTools:
 
-
     def argument_word_match(self, sentences):
-
 
         argumentative_words = ['argue', 'conclude', 'therefore']
         matches = []
-        threshold = 0.8
         for sentence in sentences:
             for arg in argumentative_words:
                 if arg in sentence:
                     matches.append(sentence)
         print(matches)
+
 
 if __name__ == '__main__':
     main = MiningTools()
